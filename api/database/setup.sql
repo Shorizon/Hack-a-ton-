40271dc3cd0b8738ee11e917861a6a94ea6159f6
shojib deb <shodeb123@gmail.com>
2023-02-10 11:17:44 +0000

    ADD: new field for db table

--- api/database/setup.sql
+++ api/database/setup.sql
@@ -4,20 +4,24 @@ CREATE TABLE diary (
     diary_id INT GENERATED ALWAYS AS IDENTITY,
     diary_name VARCHAR(30) UNIQUE NOT NULL,
     diary_content VARCHAR(500) NOT NULL,
-    diary_timestamp timestamp default current_timestamp
+    diary_timestamp timestamp default current_timestamp,
+    diary_category VARCHAR(30) NOT NULL
 );
 
 INSERT INTO
     diary (
         diary_name,
-        diary_content
+        diary_content,
+        diary_category
     )
 VALUES
     (
         'day 1',
-        'I have created a diary'
+        'I have created a diary',
+        'fun'
     ),
     (
         'day 2',
-        'I have created a diary part 2'
+        'I have created a diary part 2',
+        'work'
     )
